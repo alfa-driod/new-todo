e838f7d6f9f4cfdaed4870b925fd0890818d8a95
prince <princebig12345@gmail.com>
2021-12-21 14:57:53 +0000

    todo app

---
+++ express_app.js
@@ -0,0 +1,32 @@
+
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const todoController = require('./controllers/todoController');
+const server = express();
+const mongo_db_url ='';
+const mongodb_db_url="";
+
+server.use(express.json());
+
+
+server.listen(4000, function(){
+    console.log('Server has started to run in express');
+    mongoose.connect(process.env.LOCAL_URL)
+    .then(function(){
+        console.log('DB is connected');
+        server.get('/todos', todoController.getAllTodos);
+        server.get('/todos/:id', todoController.getTodoById);
+
+        server.post('/todos',todoController.insertTodo);
+        server.put('/todos/:id',  todoController.updateTodoById);
+        server.delete('/todos/:id', todoController.deleteTodoById);
+
+    })
+    
+    .catch(function(error){
+        console.log('DB is not connected:',error.message);
+
+    });
+});
