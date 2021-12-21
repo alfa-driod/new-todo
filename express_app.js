2cd6f0960c17b315ad07ffdfdc6cd6c9e2da5ff5
prince <princebig12345@gmail.com>
2021-12-21 16:54:16 +0000

    https://github.com/alfa-driod/new-todo.git

--- express_app.js
+++ express_app.js
@@ -12,18 +12,23 @@ const PORT=process.env.PORT|| 4000;
 
 server.use(express.json());
 
+server.get('/',function(req,res){
+    res.status(200).json({
+        success:true,message:"welcome this is my todo app"
+    })
+})
+server.get('/todos', todoController.getAllTodos);
+server.get('/todos/:id', todoController.getTodoById);
+
+server.post('/todos',todoController.insertTodo);
+server.put('/todos/:id',  todoController.updateTodoById);
+server.delete('/todos/:id', todoController.deleteTodoById);
 
 server.listen(PORT, function(){
     console.log('Server has started to run in express');
     mongoose.connect(process.env.LOCAL_URL)
     .then(function(){
         console.log('DB is connected');
-        server.get('/todos', todoController.getAllTodos);
-        server.get('/todos/:id', todoController.getTodoById);
-
-        server.post('/todos',todoController.insertTodo);
-        server.put('/todos/:id',  todoController.updateTodoById);
-        server.delete('/todos/:id', todoController.deleteTodoById);
 
     })
     
